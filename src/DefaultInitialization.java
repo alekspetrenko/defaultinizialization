f5876e18c5cb72feed8fdd2ad3b12679bd4f44d6
Aleksandr Petrenko <petrenko.aleksandr025@gmail.com>
2016-08-18 20:56:10 +0300

    Defaultinizialization

---
+++ src/DefaultInitialization.java
@@ -0,0 +1,12 @@
+
+public class DefaultInitialization {
+	static int a;
+	static char x;
+	public static void main(String[] args) {
+		System.out.println("a = " + a);
+		System.out.println("x = [" + x + ']');
+		// TODO Auto-generated method stub
+
+	}
+
+}
